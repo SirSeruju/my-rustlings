2bb9329a2ab0a2cd991253daaccab1e9147751c6
Seruju <sir.serujU@yandex.ru>
2023-11-18 22:51:22 +0000

    Answers

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,29 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+
+        let mut values = s.split(",");
+        let name = match values.next() {
+            Some(x) => x,
+            None => return Person::default()
+        };
+        if name.len() == 0 {
+            return Person::default();
+        }
+
+        let age = match values.next() {
+            Some(x) => x,
+            None => return Person::default()
+        };
+        match age.parse::<usize>() {
+            Ok(age) => Person { name: name.to_string(), age },
+            Err(_) => Person::default()
+        }
     }
 }
 
--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -27,8 +27,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
 // integers, an array of three integers, and a slice of integers.
@@ -41,6 +39,20 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        Ok(Color {
+            red: match tuple.0.try_into() {
+                Ok(x) => x,
+                Err(_) => return Err(IntoColorError::IntConversion)
+            },
+            green: match tuple.1.try_into() {
+                Ok(x) => x,
+                Err(_) => return Err(IntoColorError::IntConversion)
+            },
+            blue: match tuple.2.try_into() {
+                Ok(x) => x,
+                Err(_) => return Err(IntoColorError::IntConversion)
+            }
+        })
     }
 }
 
@@ -48,6 +60,20 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        Ok(Color {
+            red: match arr[0].try_into() {
+                Ok(x) => x,
+                Err(_) => return Err(IntoColorError::IntConversion)
+            },
+            green: match arr[1].try_into() {
+                Ok(x) => x,
+                Err(_) => return Err(IntoColorError::IntConversion)
+            },
+            blue: match arr[2].try_into() {
+                Ok(x) => x,
+                Err(_) => return Err(IntoColorError::IntConversion)
+            }
+        })
     }
 }
 
@@ -55,6 +81,24 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+        let mut ss = slice.iter();
+        Ok(Color {
+            red: match (*ss.next().expect("unreachable")).try_into() {
+                Ok(x) => x,
+                Err(_) => return Err(IntoColorError::IntConversion)
+            },
+            green: match (*ss.next().expect("unreachable")).try_into() {
+                Ok(x) => x,
+                Err(_) => return Err(IntoColorError::IntConversion)
+            },
+            blue: match (*ss.next().expect("unreachable")).try_into() {
+                Ok(x) => x,
+                Err(_) => return Err(IntoColorError::IntConversion)
+            }
+        })
     }
 }
 
